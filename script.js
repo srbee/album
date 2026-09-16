cad00d2037460c504aa50518b9a95fcbcd8a6091
Sudhir Bhide <srbhide@yahoo.com>
2026-09-16 20:19:00 +0530

    Fix photo count message in script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function loadNextPhoto() {
             : "";
 
         photoCount.textContent = loadedPhotos +
-            (loadedPhotos === 1 ? " छायाचित्र लोड हो रहे है" : " छायाचित्र लोड हो रहे है");
+            (loadedPhotos === 1 ? " छायाचित्र " : " छायाचित्र ");
     };
 
     img.src = imagePath;
