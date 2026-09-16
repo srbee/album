8a50cd55c0b1869895fc2ba8790214c66e5e6828
Sudhir Bhide <srbhide@yahoo.com>
2026-09-16 20:07:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function loadNextPhoto() {
             : "";
 
         photoCount.textContent = loadedPhotos +
-            (loadedPhotos === 1 ? " छायाचित्र" : " छायाचित्र");
+            (loadedPhotos === 1 ? " छायाचित्र लोड हो रहे है" : " छायाचित्र लोड हो रहे है");
     };
 
     img.src = imagePath;
